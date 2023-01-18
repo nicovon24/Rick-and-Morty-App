33ec0f963ed1d7db708520f2e088e54269b2134d
nicovon24 <nicovon24@gmail.com>
2023-01-18 14:44:51 -0300

    commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,23 +4,33 @@ import styles from "./Login.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faXmark } from '@fortawesome/free-solid-svg-icons'
 import {users_data} from "../../data/users_data.js"
+import { NavLink } from "react-router-dom"
 
 export default function Login(){
     let [name, setName] = useState("")
     let [password, setPassword] = useState("")
     let [isHiddenActive, setIsHiddenActive] = useState(true)
     let [isIncorrectData, setIsIncorrectData] = useState("")
+    let [isAdmin, setIsAdmin] = useState(false)
 
     let homeNavigate = useNavigate()
 
     {/* hacer parte de errores */}
     const handleSubmitForm = e=>{
         e.preventDefault()
-        let users = users_data.data
-        if(users[name]){
-            if(users[name].password===password){
-                setIsIncorrectData(false)
-                homeNavigate("/home")
+        if(!isAdmin){
+            let users = users_data.data
+            if(users[name]){
+                if(users[name].password===password){
+                    setIsIncorrectData(false)
+                    homeNavigate("/home")
+                }
+                else{
+                    setIsIncorrectData(true)
+                    setTimeout(()=>{
+                        setIsIncorrectData(false)
+                    }, [5000])
+                }
             }
             else{
                 setIsIncorrectData(true)
@@ -30,10 +40,7 @@ export default function Login(){
             }
         }
         else{
-            setIsIncorrectData(true)
-            setTimeout(()=>{
-                setIsIncorrectData(false)
-            }, [5000])
+            homeNavigate("/home")
         }
     }
 
@@ -53,13 +60,14 @@ export default function Login(){
                 </div>
 
                 <div className={styles.checkbox_container}>
-                    <input type="checkbox" name="" id="" />
-                    <label>Remember me?</label>
+                    <input type="checkbox" name="cb_admin" for="cb_admin" id="cb_admin" value={isAdmin} onChange={()=>setIsAdmin(prev=>!prev)} />
+                    <label name="cb_admin" htmlFor="cb_admin">Login as admin (do not need login data)</label>
                 </div>
 
                 {isIncorrectData ? <p className={styles.incorrect_data}>Incorrect data, try again</p> : ""}
 
                 <button className={`${styles.btn_submit}`} type="submit"><label>Login</label></button>
+                
             </form>
 
             <div className={styles.final_message}>
